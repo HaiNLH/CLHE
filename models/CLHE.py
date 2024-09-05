ee99640749c1c85796b03765b1ae12ec9a6704d0
hainlh <87404207+HaiNLH@users.noreply.github.com>
2024-09-05 19:09:42 +0700

    Trying directly import cate

--- models/CLHE.py
+++ models/CLHE.py
@@ -2,7 +2,6 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import scipy.sparse as sp
 from models.utils import TransformerEncoder
 from collections import OrderedDict
 
@@ -45,11 +44,8 @@ class HierachicalEncoder(nn.Module):
         self.num_user = self.conf["num_users"]
         self.num_bundle = self.conf["num_bundles"]
         self.num_item = self.conf["num_items"]
-        self.num_cate = self.conf["num_cates"]
-        
         self.embedding_size = 64
-        self.ui_graph, self.bi_graph_train, self.bi_graph_seen, self.ic_graph = raw_graph
-        print(self.ic_graph)
+        self.ui_graph, self.bi_graph_train, self.bi_graph_seen = raw_graph
         self.attention_components = self.conf["attention"]
 
         self.content_feature, self.text_feature, self.cf_feature = features
@@ -227,13 +223,12 @@ class CLHE(nn.Module):
         self.conf = conf
         device = self.conf["device"]
         self.device = device
-        self.raw_graph = raw_graph
         self.num_user = self.conf["num_users"]
         self.num_bundle = self.conf["num_bundles"]
         self.num_item = self.conf["num_items"]
-        self.num_cate = self.conf["num_cates"]
+        self.num_cate  = self.conf["num_cates"]
         self.embedding_size = 64
-        self.ui_graph, self.bi_graph_train, self.bi_graph_seen, self.ic_graph= raw_graph
+        self.ui_graph, self.bi_graph_train, self.bi_graph_seen, self.ic_graph = raw_graph
         self.item_augmentation = self.conf["item_augment"]
 
         self.encoder = HierachicalEncoder(conf, raw_graph, features)
@@ -249,7 +244,7 @@ class CLHE(nn.Module):
 
         self.cl_temp = conf['cl_temp']
         self.cl_alpha = conf['cl_alpha']
- 
+
         self.bundle_cl_temp = conf['bundle_cl_temp']
         self.bundle_cl_alpha = conf['bundle_cl_alpha']
         self.cl_projector = nn.Linear(self.embedding_size, self.embedding_size)
@@ -259,7 +254,13 @@ class CLHE(nn.Module):
             self.dropout = nn.Dropout(p=self.dropout_rate)
         elif self.item_augmentation in ["FN"]:
             self.noise_weight = conf['noise_weight']
+        self.init_emb()
+        self.item_cate_feat = self.ic_graph @ self.cate_feature
+        self.item_cate_feat = F.normalize(self.item_cate_feat, dim = -1)
 
+    def init_emb(self):
+        self.cate_feature = nn.Parameter(torch.FloatTensor(self.num_cate, self.embedding_size))
+    
     def forward(self, batch):
         idx, full, seq_full, modify, seq_modify = batch  # x: [bs, #items]
         mask = seq_full == self.num_item
@@ -272,7 +273,7 @@ class CLHE(nn.Module):
 
         # compute loss >>>
         logits = bundle_feature @ feat_retrival_view.transpose(0, 1)
-        loss = recon_loss_function(logits, full)  # main_loss 
+        loss = recon_loss_function(logits, full)  # main_loss
 
         # # item-level contrastive learning >>>
         items_in_batch = torch.argwhere(full.sum(dim=0)).squeeze()
@@ -280,7 +281,6 @@ class CLHE(nn.Module):
         if self.cl_alpha > 0:
             if self.item_augmentation == "FD":
                 item_features = self.encoder(batch, all=True)[items_in_batch]
-                #make it go through Light-GCN
                 sub1 = self.cl_projector(self.dropout(item_features))
                 sub2 = self.cl_projector(self.dropout(item_features))
                 item_loss = self.cl_alpha * cl_loss_function(
@@ -288,17 +288,10 @@ class CLHE(nn.Module):
             elif self.item_augmentation == "NA":
                 item_features = self.encoder(batch, all=True)[items_in_batch]
                 item_loss = self.cl_alpha * cl_loss_function(
-                item_features.view(-1, self.embedding_size), item_features.view(-1, self.embedding_size), self.cl_temp)
+                    item_features.view(-1, self.embedding_size), item_features.view(-1, self.embedding_size), self.cl_temp)
             elif self.item_augmentation == "FN":
-                item_ft = self.encoder(batch, all=True)
-                tmp = BunCa(self.conf, self.raw_graph, item_ft,self.device).propagate()
-                # print(tmp.shape)
-                # print(item_ft.shape)
-                # print(items_in_batch.long)
-                item_features = tmp[items_in_batch]
-                # item_f = item_ft[items_in_batch]
-                # print(item_features.shape)
-                # print(item_f.shape)
+                item_features = self.encoder(batch, all=True)[items_in_batch]
+                item_features = item_features*self.item_cate_feat
                 sub1 = self.cl_projector(
                     self.noise_weight * torch.randn_like(item_features) + item_features)
                 sub2 = self.cl_projector(
@@ -307,19 +300,8 @@ class CLHE(nn.Module):
                     sub1.view(-1, self.embedding_size), sub2.view(-1, self.embedding_size), self.cl_temp)
             elif self.item_augmentation == "MD":
                 sub1, sub2 = self.encoder.generate_two_subs(self.dropout_rate)
-                bunca_model1 = BunCa(self.conf, self.raw_graph, sub1)
-                bunca_model2 = BunCa(self.conf, self.raw_graph,sub2)
-                items1_feature = bunca_model1.propagate()
-                items2_feature = bunca_model2.propagate()
-                
-                sub1 = items1_feature[items_in_batch]
-                sub2 = items2_feature[items_in_batch]
-                # Process features as needed
-                # combined_feature = torch.cat([bundles_feature[0], items_feature[0]], dim=-1)
-                # Pass through additional layers
-                # output = self.fc(combined_feature)
-
-
+                sub1 = sub1[items_in_batch]
+                sub2 = sub2[items_in_batch]
                 item_loss = self.cl_alpha * cl_loss_function(
                     sub1.view(-1, self.embedding_size), sub2.view(-1, self.embedding_size), self.cl_temp)
         # # item-level contrastive learning <<<
@@ -355,170 +337,3 @@ class CLHE(nn.Module):
 
     def propagate(self, test=False):
         return None
-
-
-def laplace_transform(graph):
-    rowsum_sqrt = sp.diags(1 / (np.sqrt(graph.sum(axis=1).A.ravel()) + 1e-8))
-    colsum_sqrt = sp.diags(1 / (np.sqrt(graph.sum(axis=0).A.ravel()) + 1e-8))
-    graph = rowsum_sqrt @ graph @ colsum_sqrt
-
-    return graph
-
-
-def to_tensor(graph):
-    graph = graph.tocoo()
-    values = graph.data
-    indices = np.vstack((graph.row, graph.col))
-    graph = torch.sparse.FloatTensor(torch.LongTensor(indices), torch.FloatTensor(values), torch.Size(graph.shape))
-
-    return graph
-
-
-def np_edge_dropout(values, dropout_ratio):
-    mask = np.random.choice([0, 1], size=(len(values),), p=[dropout_ratio, 1 - dropout_ratio])
-    values = mask * values
-    return values
-
-#Idea is to reconstruct the part getting item embedding learning from connection U-C C-I => U-I, B-U, U-I
-#Getting item rep after using self attention => Information from User and Bundle and Category
-class BunCa(nn.Module):
-    def __init__(self,conf, raw_graph, items_feat,device):
-        super().__init__()
-        self.conf = conf
-        self.device = device
-
-        self.embedding_size = conf["embedding_size"]
-        self.embed_L2_norm = conf["l2_reg"]
-        self.num_users = conf["num_users"]
-        self.num_bundles = conf["num_bundles"]
-        self.num_items = conf["num_items"]
-        self.num_cates = conf["num_cates"]
-        self.items_feature  = items_feat
-
-
-        self.ui_graph, self.bi_graph_train, self.bi_graph_seen, self.ic_graph= raw_graph
-        self.bc_graph = self.bi_graph_train@self.ic_graph
-        self.num_layers = self.conf["num_layers"]
-        self.init_emb()
-        self.init_md_dropouts()
-        self.get_item_agg_graph()
-        self.get_item_agg_graph()
-
-    def init_md_dropouts(self):
-        self.item_level_dropout = nn.Dropout(0.2, True)
-        self.bundle_level_dropout = nn.Dropout(0.2, True)
-        self.bundle_agg_dropout = nn.Dropout(0.2, True)
-
-    def init_emb(self):
-        self.users_feature = nn.Parameter(torch.FloatTensor(self.num_users, self.embedding_size))
-        nn.init.xavier_normal_(self.users_feature)
-        self.bundles_feature = nn.Parameter(torch.FloatTensor(self.num_bundles, self.embedding_size))
-        nn.init.xavier_normal_(self.bundles_feature)
-        # self.items_feature = nn.Parameter(torch.FloatTensor(self.num_items, self.embedding_size))
-        # nn.init.xavier_normal_(self.items_feature)
-        self.cates_feature = nn.Parameter(torch.FloatTensor(self.num_cates, self.embedding_size))
-        nn.init.xavier_normal_(self.cates_feature)
-
-    def get_cate_level_graph(self):
-        bc_graph = self.bc_graph
-        ic_graph = self.ic_graph
-        device = self.device
-        cate_level_graph = sp.bmat([[sp.csr_matrix((bc_graph.shape[0], bc_graph.shape[0])),bc_graph],
-                                    [bc_graph.T, sp.csr_matrix((bc_graph.shape[1],bc_graph.shape[1]))]])
-        ic_propagate_graph = sp.bmat([[sp.csr_matrix((ic_graph.shape[0], ic_graph.shape[0])), ic_graph],
-                                      [ic_graph.T, sp.csr_matrix((ic_graph.shape[1], ic_graph.shape[1]))]])
-        self.ic_propagate_graph_ori = to_tensor(laplace_transform(ic_propagate_graph)).to(device)
-        self.cate_level_graph = to_tensor(laplace_transform(cate_level_graph)).to(device)
-        self.ic_propagate_graph = to_tensor(laplace_transform(ic_propagate_graph)).to(device)
-
-    def get_item_level_graph(self, threshold=0):
-        bi_graph = self.bi_graph_train
-        device = self.device
-        bb_graph = (bi_graph @ bi_graph.T) >threshold
-        ii_graph = (bi_graph.T @ bi_graph) > threshold
-        item_level_graph = sp.bmat([[bb_graph, bi_graph],
-                                    [bi_graph.T, ii_graph]])
-        
-        self.item_level_graph = to_tensor(laplace_transform(item_level_graph)).to(device)
-        return self.item_level_graph
-
-    def get_item_agg_graph(self):
-        ic_graph = self.ic_graph
-        device = self.device
-
-        item_size  = ic_graph.sum(axis=1) +1e-8
-        ic_graph = sp.diags(1/item_size.A.ravel())@ ic_graph
-        self.item_agg_graph = to_tensor(ic_graph).to(device)
-
-    def get_bundle_agg_graph(self):
-        bc_graph  = self.bc_graph
-        device = self.device
-
-        bundle_size = bc_graph.sum(axis = 1) + 1e-8
-        bc_graph = sp.diags(1/bundle_size.A.ravel())@bc_graph
-        self.bundle_agg_graph = to_tensor(bc_graph).to(device)
-
-    def one_propagate(self, graph, A_feature, B_feature, mess_dropout, test, coefs=None):
-        A_feature = A_feature.to(self.device)
-        B_feature = B_feature.to(self.device)
-        # print("a_feature: ",A_feature.device)
-        # print("b_feature: ",B_feature.device)
-       
-        features = torch.cat((A_feature, B_feature), 0)
-        all_features = [features]
-
-        for i in range(self.num_layers):
-          features  = features.to(self.device)
-          # print(features.device)
-          # print(graph.device)
-          features = torch.spmm(graph, features)
-          features = features / (i + 2)
-          all_features.append(F.normalize(features, p=2, dim=1))
-        for i in range(len(all_features)):
-          all_features[i] = all_features[i].to(self.device)
-          # print(all_features[i].device)
-        all_features = torch.stack(all_features, 1).to(self.device)
-        # print("all_feature: ", all_features.device)
-        if coefs is not None:
-            all_features = all_features * coefs
-            # print("all_feature2: ",all_features.device)
-        all_features = torch.sum(all_features, dim=1).squeeze(1)
-
-        A_feature, B_feature = torch.split(all_features, (A_feature.shape[0], B_feature.shape[0]), 0)
-
-        return A_feature, B_feature
-
-    def get_CL_item_rep(self, CL_cates_feature,test):
-        if test:
-            CL_items_feature = torch.matmul(self.item_agg_graph, CL_cates_feature)
-        else:
-            CL_items_feature = torch.matmul(self.item_agg_graph, CL_cates_feature)
-        # simple embedding dropout on bundle embeddings
-        # if self.conf["bundle_agg_ratio"] != 0 and self.conf["aug_type"] == "MD" and not test:
-        #    CL_items_feature = self.item_agg_dropout(CL_items_feature)
-        return CL_items_feature
-    
-    
-    def propagate(self, test = False):
-        #______________CATE level _______________
-        # if test:
-        #     CL_bundles_feature, CL_cates_feature = self.one_propagate(self.cate_level_graph, self.bundles_feature, self.cates_feature, self.item_level_dropout, test)
-        # else:
-        self.get_cate_level_graph()
-        CL_bundles_feature, CL_cates_feature = self.one_propagate(self.cate_level_graph, self.bundles_feature, self.cates_feature, self.item_level_dropout, test)
-        
-        CL_items_feature = self.get_CL_item_rep(CL_cates_feature,test)
-
-        #_____________ITEM level_________________
-        # if test:
-        #     IL_bundles_feature, IL_item_features = self.one_propagate(self.get_item_level_graph_ori, self.bundles_feature, self.items_feature, self.bundle_level_dropout, test)
-        # else:
-        self.get_item_level_graph()
-        IL_bundles_feature, IL_item_features = self.one_propagate(self.item_level_graph, self.bundles_feature, self.items_feature, self.bundle_level_dropout, test)
-        
-        # bundles_feature = [CL_bundles_feature, IL_bundles_feature]
-        # items_feature  = [CL_items_feature, IL_item_features]
-
-        # bundles_feature = CL_bundles_feature @ W_bundle1 + IL_bundles_feature @ W_bundle2
-        items_feature = CL_items_feature * 0.6 + IL_item_features * 0.4
-        return items_feature
