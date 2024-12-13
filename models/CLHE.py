dead0466531802c7c31c5fab31554bfd7bbb8b75
hainlh <87404207+HaiNLH@users.noreply.github.com>
2024-12-13 15:37:40 +0700

    Update CLHE.py

--- models/CLHE.py
+++ models/CLHE.py
@@ -484,57 +484,56 @@ class CLHE(nn.Module):
         return logits
 
     def propagate(self, test=False):
+        # a = 0.8
+        # # Perform GAT convolution
+        # cate_feat, _ = self.cbc_gat_conv(self.cate_feature, self.cbc_edge_index, return_attention_weights=True)
 
-        a = 0.8
-        # Perform GAT convolution
-        cate_feat, _ = self.cbc_gat_conv(self.cate_feature, self.cbc_edge_index, return_attention_weights=True)
-
-        # Weighted combination
-        cate_ft = cate_feat * a + self.cate_feature * (1 - a)
-        # cate_ft = torch.nan_to_num(cate_ft, nan=0.0)  # Handle NaNs explicitly
+        # # Weighted combination
+        # cate_ft = cate_feat * a + self.cate_feature * (1 - a)
+        # # cate_ft = torch.nan_to_num(cate_ft, nan=0.0)  # Handle NaNs explicitly
         
 
-        # Aggregate category to item
-        cl_item_cate = self.get_CL_item_rep(cate_ft, test)
-        #debugging
-        # print("Checking input tensors...")
-        # print("self.cate_feature NaNs:", torch.isnan(self.cate_feature).any())
-        # print("cate_feat NaNs after GAT:", torch.isnan(cate_feat).any())
-        # print("cate_ft NaNs after combination:", torch.isnan(cate_ft).any())
-        # print("cl_item_cate NaNs:", torch.isnan(cl_item_cate).any())
+        # # Aggregate category to item
+        # cl_item_cate = self.get_CL_item_rep(cate_ft, test)
+        # #debugging
+        # # print("Checking input tensors...")
+        # # print("self.cate_feature NaNs:", torch.isnan(self.cate_feature).any())
+        # # print("cate_feat NaNs after GAT:", torch.isnan(cate_feat).any())
+        # # print("cate_ft NaNs after combination:", torch.isnan(cate_ft).any())
+        # # print("cl_item_cate NaNs:", torch.isnan(cl_item_cate).any())
 
-        return cl_item_cate
+        # return cl_item_cate
         
-class Amatrix(nn.Module):
-    def __init__(self, in_dim, out_dim, n_layer=1, dropout=0.0, heads=2, concat=False, self_loop=True,
-                 extra_layer=False):
-        super(Amatrix, self).__init__()
-        self.num_layer = n_layer
-        self.dropout = dropout
-        self.in_dim = in_dim
-        self.out_dim = out_dim
-        self.heads = heads
-        self.concat = concat
-        self.self_loop = self_loop
-        self.extra_layer = extra_layer
-        self.convs = nn.ModuleList([AsymMatrix(in_channels=self.in_dim,
-                                               out_channels=self.out_dim,
-                                               dropout=self.dropout,
-                                               heads=self.heads,
-                                               concat=self.concat,
-                                               add_self_loops=self.self_loop,
-                                               extra_layer=self.extra_layer)
-                                    for _ in range(self.num_layer)])
+# class Amatrix(nn.Module):
+#     def __init__(self, in_dim, out_dim, n_layer=1, dropout=0.0, heads=2, concat=False, self_loop=True,
+#                  extra_layer=False):
+#         super(Amatrix, self).__init__()
+#         self.num_layer = n_layer
+#         self.dropout = dropout
+#         self.in_dim = in_dim
+#         self.out_dim = out_dim
+#         self.heads = heads
+#         self.concat = concat
+#         self.self_loop = self_loop
+#         self.extra_layer = extra_layer
+#         self.convs = nn.ModuleList([AsymMatrix(in_channels=self.in_dim,
+#                                                out_channels=self.out_dim,
+#                                                dropout=self.dropout,
+#                                                heads=self.heads,
+#                                                concat=self.concat,
+#                                                add_self_loops=self.self_loop,
+#                                                extra_layer=self.extra_layer)
+#                                     for _ in range(self.num_layer)])
 
-    def forward(self, x, edge_index, return_attention_weights=True):
-        feats = [x]
-        attns = []
+#     def forward(self, x, edge_index, return_attention_weights=True):
+#         feats = [x]
+#         attns = []
 
-        for conv in self.convs:
-            x, attn = conv(x, edge_index, return_attention_weights=return_attention_weights)
-            feats.append(x)
-            attns.append(attn)
+#         for conv in self.convs:
+#             x, attn = conv(x, edge_index, return_attention_weights=return_attention_weights)
+#             feats.append(x)
+#             attns.append(attn)
 
-        feat = torch.stack(feats, dim=1)
-        x = torch.mean(feat, dim=1)
-        return x, attns
+#         feat = torch.stack(feats, dim=1)
+#         x = torch.mean(feat, dim=1)
+#         return x, attns
