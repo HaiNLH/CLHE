c979b54a2f66a6ccba8870ee27fae7fe116055ef
hainlh <longhai230303@gmail.com>
2025-03-09 10:01:22 +0700

    minor fix

--- models/CrossAttention.py
+++ models/CrossAttention.py
@@ -14,8 +14,8 @@ class Cross_Attn(nn.Module):
         self.d_t, self.d_m, self.d_c = 64, 64, 64
         #3 modality: text, media, user-item:c
         self.t_only = True   # Use only text modality
-        self.m_only = False   # Use only media modality
-        self.c_only = False   # Use only user-item (content) modality
+        self.m_only = True   # Use only media modality
+        self.c_only = True   # Use only user-item (content) modality
         self.num_heads = 4    # Number of attention heads (try 4 or 8 as a starting point)
         self.layers = 2       # Number of transformer layer
         self.attn_dropout = 0.1      # Overall attention dropout rate
